50639d46c303a9efd24d6576d0e3d45b63901d37
Rafael Barbosa <rmelobarbosa@gmail.com>
2022-10-18 18:35:32 -0300

    first upload

---
+++ data.js
@@ -0,0 +1,32 @@
+// ALTERAVEL
+export default {
+  "title": "Ilha Food Park",
+  "bannerUrlDesktop": "https://rmelobarbosa.github.io/testjsonenabled/ilhafood/banner-ilha-food-park.png",
+  "bannerUrlMobile": "https://rmelobarbosa.github.io/testjsonenabled/ilhafood/banner-ilha-food-park-mobile.png",
+  "organizations": [
+   {
+      "tags": ["restaurante"],
+      "suffix": "oxentemix"
+    },
+    {
+      "tags": ["hotdog"],
+      "suffix": "catioroaracagy"
+    },
+    {
+      "tags": ["hotdog", "hamburguer"],
+      "suffix": "drsandubaaracagy"
+    },
+    {
+      "tags": ["pizza, massas"],
+      "suffix": "pizzasunset"
+    },
+    {
+      "tags": ["pastel"],
+      "suffix": "pastelandiaslz"
+    },
+	{
+      "tags": ["massas"],
+      "suffix": "nationsfoodslz"
+    }
+  ]
+}
