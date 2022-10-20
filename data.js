64e7fffb0559e616974b367cedc05b4123efe919
Rafael Barbosa <rmelobarbosa@gmail.com>
2022-10-20 14:16:41 -0300

    update title

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // ALTERAVEL
 export default {
-  "title": "Ilha Food Park",
+  "title": "Super Centro Boqueirão",
   "bannerUrlDesktop": "https://rmelobarbosa.github.io/demostore1/supercentro2.png",
   "bannerUrlMobile": "https://rmelobarbosa.github.io/demostore1/supercentro1.png",
   "organizations": [
