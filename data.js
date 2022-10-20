7655fe3452b6ca1f1a5b438a57b1ce3332311e91
isaquepontesdev <111371266+isaquepontesdev@users.noreply.github.com>
2022-10-20 17:14:34 -0300

    Inclusão de loja.

--- data.js
+++ data.js
@@ -24,11 +24,11 @@ export default {
       "tags": ["pastel"],
       "suffix": "pastelandiaslz"
     },
-	{
+    {
       "tags": ["massas"],
       "suffix": "nationsfoodslz"
     },
-	  {
+    {
       "tags": ["hotdog"],
       "suffix": "catioroaracagy"
     }
