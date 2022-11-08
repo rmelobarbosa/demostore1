373b03c3da9723043d0e472ab78f7d62b32ba3ba
W7 Company <116319067+W7Company@users.noreply.github.com>
2022-11-08 10:49:49 -0300

    Mudança de nome - Ribeirão Pires

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // ALTERAVEL
 export default {
-  "title": "Super Centro Boqueirão",
+  "title": "Estância Turística de Ribeirão Pires",
   "bannerUrlDesktop": "https://rmelobarbosa.github.io/demostore1/ribeiraopirestela.png",
   "bannerUrlMobile": "https://rmelobarbosa.github.io/demostore1/ribeiraopiresmobile.png",
   "organizations": [
