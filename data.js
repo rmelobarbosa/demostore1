427384feb85895eb96b7553adfdd554f9d5829e0
W7 Company <116319067+W7Company@users.noreply.github.com>
2023-03-02 21:19:32 -0300

    Update data.js
    mod itarare

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 // ALTERAVEL
 export default {
-  "title": "Estância Turística de Ribeirão Pires",
-  "bannerUrlDesktop": "https://rmelobarbosa.github.io/demostore1/ribeiraopirestela.png",
-  "bannerUrlMobile": "https://rmelobarbosa.github.io/demostore1/ribeiraopiresmobile.png",
+  "title": "Itararé",
+  "bannerUrlDesktop": "https://rmelobarbosa.github.io/demostore1/bannertelaITARARE.png",
+  "bannerUrlMobile": "https://rmelobarbosa.github.io/demostore1/bannermobileITARARE.png",
   "organizations": [
    {
       "tags": ["restaurante"],
